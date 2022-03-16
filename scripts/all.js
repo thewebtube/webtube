b0fd79bb113aed22dc1b62c847404f0e4c852b4b
andronedev <31452517+andronedev@users.noreply.github.com>
2022-03-16 21:03:10 +0100

    remove background js

--- scripts/all.js
+++ scripts/all.js
@@ -21,4 +21,3 @@ function fetchInject(url){
 fetchInject("https://raw.githubusercontent.com/andronedev/webtube/main/scripts/adguard.js")
 fetchInject("https://raw.githubusercontent.com/andronedev/webtube/main/scripts/ryd.js")
 fetchInject("https://raw.githubusercontent.com/andronedev/webtube/main/scripts/nsfbypass.js")
-fetchInject("https://raw.githubusercontent.com/andronedev/webtube/main/scripts/background.js")
