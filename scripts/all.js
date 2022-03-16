50d0cd435a2889e4fe633f07f43e15938cc7987e
andronedev <31452517+andronedev@users.noreply.github.com>
2022-03-16 17:34:28 +0100

    up

---
+++ scripts/all.js
@@ -0,0 +1,21 @@
+function fetchInject(url){
+  (() => {
+    async function runShortcut() {
+      try {
+        const response = await fetch(url);
+        const text = await response.text();
+        try {
+          eval(text);
+        } catch (ex) {
+          completion(ex);
+        }
+      } catch (ex) {
+        completion(ex.toString());
+      }
+    }
+    runShortcut();
+  })();
+}
+
+
+fetchInject("https://raw.githubusercontent.com/andronedev/webtube/main/scripts/adguard.js")
