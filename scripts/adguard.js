853c72cab7fe5f14f22c719c181f6d63cf3e59ab
andronedev <31452517+andronedev@users.noreply.github.com>
2022-03-16 20:11:27 +0100

    change title

--- scripts/adguard.js
+++ scripts/adguard.js
@@ -22,67 +22,67 @@
     function runBlockYoutube() {
       const locales = {
         en: {
-          logo: "with&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "The shortcut has already been executed.",
           wrongDomain: "This shortcut is supposed to be launched only on YouTube.",
           success: "YouTube is now ad-free! Please note that you need to run this shortcut again if you reload the page."
         },
         ru: {
-          logo: "с&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "Быстрая команда уже выполнена.",
           wrongDomain: "Эта быстрая команда предназначена для использования только на YouTube.",
           success: "Теперь YouTube без рекламы! Важно: при перезагрузке страницы вам нужно будет заново запустить команду."
         },
         es: {
-          logo: "con&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "El atajo ya ha sido ejecutado.",
           wrongDomain: "Se supone que este atajo se lanza sólo en YouTube.",
           success: "¡YouTube está ahora libre de anuncios! Ten en cuenta que tienes que volver a ejecutar este atajo si recargas la página."
         },
         de: {
-          logo: "mit&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "Der Kurzbefehl wurde bereits ausgeführt.",
           wrongDomain: "Dieser Kurzbefehl soll nur auf YouTube gestartet werden.",
           success: "YouTube ist jetzt werbefrei! Bitte beachten Sie, dass Sie diesen Kurzbefehl erneut ausführen müssen, wenn Sie die Seite neu laden."
         },
         fr: {
-          logo: "avec&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "Le raccourci a déjà été exécuté.",
           wrongDomain: "Ce raccourci est censé d’être lancé uniquement sur YouTube.",
           success: "YouTube est maintenant libre de pub ! Veuillez noter qu’il faudra rééxecuter le raccourci si vous rechargez la page."
         },
         it: {
-          logo: "con&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "Il comando è già stato eseguito.",
           wrongDomain: "Questa scorciatoia dovrebbe essere lanciata solo su YouTube.",
           success: "YouTube è ora libero da pubblicità! Si prega di notare che è necessario eseguire nuovamente questa scorciatoia se ricarichi la pagina."
         },
         "zh-cn": {
-          logo: "使用&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "快捷指令已在运行",
           wrongDomain: "快捷指令只能在 YouTube 上被启动。",
           success: "现在您的 YouTube 没有广告！请注意，若您重新加载页面，您需要再次启动快捷指令。"
         },
         "zh-tw": {
-          logo: "偕同&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "此捷徑已被執行。",
           wrongDomain: "此捷徑應該只於 YouTube 上被啟動。",
           success: "現在 YouTube 為無廣告的！請注意，若您重新載入該頁面，您需要再次執行此捷徑。"
         },
         ko: {
-          logo: "AdGuard&nbsp;사용",
+          logo: "WebTube",
           alreadyExecuted: "단축어가 이미 실행되었습니다.",
           wrongDomain: "이 단축어는 YouTube에서만 사용 가능합니다.",
           success: "이제 광고없이 YouTube를 시청할 수 있습니다. 페이지를 새로고침 할 경우, 이 단축어를 다시 실행해야 합니다."
         },
         ja: {
-          logo: "AdGuard作動中",
+          logo: "WebTube",
           alreadyExecuted: "ショートカットは既に実行されています。",
           wrongDomain: "※このショートカットは、YouTubeでのみ適用されることを想定しています。",
           success: "YouTubeが広告なしになりました！※YouTubeページを再読み込みした場合は、このショートカットを再度実行する必要がありますのでご注意ください。"
         },
         uk: {
-          logo: "з&nbsp;AdGuard",
+          logo: "WebTube",
           alreadyExecuted: "Ця швидка команда вже виконується.",
           wrongDomain: "Цю швидку команду слід запускати лише на YouTube.",
           success: "Тепер YouTube без реклами! Проте після перезавантаження сторінки необхідно знову запустити цю швидку команду."
@@ -342,7 +342,7 @@
         });
       };
       const script = document.createElement("script");
-      const scriptText = pageScript.toString().replace("__logo_text__", "WebTube");
+      const scriptText = pageScript.toString().replace("__logo_text__", getMessage("logo"));
       script.innerHTML = `(${scriptText})();`;
       document.head.appendChild(script);
       document.head.removeChild(script);
